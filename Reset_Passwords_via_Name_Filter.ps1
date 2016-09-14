fc7d6eb49f4a934f0d4fa2ce670088ced9689ea7
Samantha <sam0x007fff@outlook.com>
2016-09-14 14:39:17 +0100

    Update Reset_Passwords_via_Name_Filter.ps1

--- Reset_Passwords_via_Name_Filter.ps1
+++ Reset_Passwords_via_Name_Filter.ps1
@@ -1,3 +1,3 @@
 get-aduser  -Filter {Name -like "*iPads*"} ` 
--searchbase "ou=System,ou=Users,ou=witham,ou=Federation Objects,dc=prioryacademies,dc=co,dc=uk" `
+-searchbase "ou=Users,ou=Objects,dc=mydomain,dc=co,dc=uk" `
 | Set-ADAccountPassword -NewPassword (ConvertTo-SecureString -AsPlainText "VAWw3v%$sd&" -force) -Reset
