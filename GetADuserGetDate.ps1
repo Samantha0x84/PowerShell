9dcf478fb9ffa6801a33c173b9a889c04188b909
Samantha <sam0x007fff@outlook.com>
2016-11-27 14:32:01 +0000

    Update GetADuserGetDate.ps1

--- GetADuserGetDate.ps1
+++ GetADuserGetDate.ps1
@@ -1,2 +1,2 @@
 #Get Date with Get-Aduser example
-get-aduser -seachbase "ou=foobar,dc=domain,dc=com" -Filter * -properties lastlogondate | where-object lastlogondate -lt (get-date -month 5) | select name, givenname, surname
+get-aduser -searchbase "ou=foobar,dc=domain,dc=com" -Filter * -properties lastlogondate | where-object lastlogondate -lt (get-date -month 5) | select name, givenname, surname
