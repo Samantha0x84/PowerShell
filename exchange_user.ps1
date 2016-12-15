ef095d3e7ad10af1565824d7210e58f63035a8e9
Samantha <sam0x007fff@outlook.com>
2016-12-15 09:19:21 +0000

    Update exchange_user.ps1

--- exchange_user.ps1
+++ exchange_user.ps1
@@ -13,7 +13,8 @@ Write-Host "PSSession from pexc01 already imported \n"
 Write-Host "Avaliable mailbox databases: "
 $mailboxes = Get-MailboxDatabase | Select-Object Name -ExpandProperty Name
 Echo "-------------" $mailboxes "-------------"
-$username = Read-Host "Enter the username: "
-$mailboxdatabase = "Enter the name of the mailbox: "
+$username = Read-Host "Enter the username "
+$mailboxdatabase = Read-Host "Enter the name of the mailbox "
 
 Enable-Mailbox -Identity $username -Database $mailboxdatabase
+
