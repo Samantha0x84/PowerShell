b031b8368e1377219c489e3d8b95286ebdb33e51
Samantha <sam0x007fff@outlook.com>
2016-12-15 09:15:39 +0000

    Update exchange_user.ps1

--- exchange_user.ps1
+++ exchange_user.ps1
@@ -1,18 +1,18 @@
-if ($session.ComputerName -ne "exchangeserver.mydomain.co.uk")
+if ($session.ComputerName -ne "pexc01.prioryacademies.co.uk")
 {
-Write-Host "Importing PSSession from exchangeserver.mydomain.co.uk"
-$connectExchange = "http://exchangeserver.mydomain.co.uk/powershell"
+Write-Host "Importing PSSession from pexc01"
+$connectExchange = "http://pexc01.prioryacademies.co.uk/powershell"
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $connectExchange -Authentication Kerberos
 Import-PSSession $Session -ErrorAction silentlycontinue -Verbose
 }
 else
 {
-Write-Host "PSSession from exchangeserver.mydomain.co.uk already imported \n"
+Write-Host "PSSession from pexc01 already imported \n"
 }
 
 Write-Host "Avaliable mailbox databases: "
-Get-MailboxDatabase | Select-Object Name
-
+$mailboxes = Get-MailboxDatabase | Select-Object Name -ExpandProperty Name
+Echo "-------------" $mailboxes "-------------"
 $username = Read-Host "Enter the username: "
 $mailboxdatabase = "Enter the name of the mailbox: "
 
