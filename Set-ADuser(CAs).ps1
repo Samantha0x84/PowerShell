e081c7d8a86be7f4bde460fb827386a5721c02a8
Samantha <sam0x007fff@outlook.com>
2017-01-24 12:35:58 +0000

    Create Set-ADuser(CAs).ps1

---
+++ Set-ADuser(CAs).ps1
@@ -0,0 +1,24 @@
+#We have "controlled assessment" accounts for students. However whoever set up these accounts didn't include any first names or surnames.
+#Luckly the controlled assment accounts are name the same as their usual accounts, just with a prefix of "CA"
+#This script references their normal account and pulls off their names.
+
+
+$normalAccount = get-aduser -searchbase "OU=212,OU=Some Academy,OU=Students,OU=Users,OU=Some OU,DC=mydomain,DC=co,DC=uk" | select samaccountname
+
+foreach ($normalAccount in $normalAccount)
+{
+write-host $normalAccount
+
+$examAccount  = $normalAccount | select samaccountname -ExpandProperty samaccountname
+$givenname    = get-aduser $examAccount  | select givenname -ExpandProperty givenname
+$surname      = get-aduser $examAccount  | select surname -ExpandProperty surname
+$examAccount  = "CA"+$examAccount
+$userToUpdate = get-aduser -filter {name -like $examAccount} | select samaccountname -ExpandProperty samaccountname
+
+Set-ADUser -Identity $userToUpdate -Surname $surname -GivenName $givenname
+
+
+write-host -BackgroundColor DarkRed -ForegroundColor White "`n" $userToUpdate "`n" $surname "`n"  $givenname "`n"
+write-host "-------------------------- `n"
+
+}
